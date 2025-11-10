5b934fa5cd0fa52add8514af3e4f2300826fbae6
Gowri Shankar <gowris073@gmail.com>
2025-11-10 09:22:44 +0530

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/utsav-papers/',
+  base: '/utsav-papers/', // Important for GitHub Pages
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
